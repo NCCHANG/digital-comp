167bda35b9a1ac0cfebee28109a82e30502ea77d
NCCHANG <changhoehin2005@gmail.com>
2025-09-22 14:28:51 +0800

    Enhance Location class include price and update court initialization with price; add input validation and booking prompt in main

--- main.cpp
+++ main.cpp
@@ -4,23 +4,27 @@
 
 class Location {
     private:
-    std::string address;
-    double latitude;
-    double longitude;
+        std::string address;
+        double latitude;
+        double longitude;
+        double price; //per hour
+
     public:
-    Location() : address("Unknown"), latitude(0.0), longitude(0.0) {}
-    Location(std::string address, double latitude, double longitude)
-        : address(address), latitude(latitude), longitude(longitude) {}
-    Location(double latitude, double longitude)
-        : address("Unknown"), latitude(latitude), longitude(longitude) {}
+        Location() : address("Unknown"), latitude(0.0), longitude(0.0) {}
+        Location(std::string address, double latitude, double longitude, double price)
+            : address(address), latitude(latitude), longitude(longitude), price(price) {}
+        Location(std::string address, double latitude, double longitude)
+            : address(address), latitude(latitude), longitude(longitude) {}
+        Location(double latitude, double longitude)
+            : address("Unknown"), latitude(latitude), longitude(longitude) {}
 
-    std::string getAddress() const { return address; }
-    double getLatitude() const { return latitude; }
-    double getLongitude() const { return longitude; }
+        std::string getAddress() const { return address; }
+        double getLatitude() const { return latitude; }
+        double getLongitude() const { return longitude; }
 
-    void display() const {
-        std::cout << "Latitude: " << latitude << ", Longitude: " << longitude << std::endl;
-    }
+        void display() const {
+            std::cout << "Latitude: " << latitude << ", Longitude: " << longitude << std::endl;
+        }
 };
 
 double toRadians(double degree) {
@@ -59,22 +63,28 @@ int main() {
 
     // Can integrate with Google Maps API to fetch real-time data in the future
     std::vector<Location> badmintonCourts = {
-        Location("Persiaran Multimedia, 63100 Cyberjaya, Selangor",2.9272342240047107, 101.64309256580782), // MMU Court
-        Location("Lot 5225, 4, Jalan Puchong Utama 7e, Taman Puchong Utama, 47100 Puchong, Selangor", 2.989928867751879, 101.61626328392457),  // KSL Court Puchong
-        Location("64959, Jalan CJ 15, Kawasan Perindustrian Cheras Jaya, 43200 Seri Kembangan, Selangor", 3.020870690407236, 101.76515845343319),    // Cheras Court
-        Location("Sunsuria Avenue, Persiaran Mohagani, Kota Damansara, 47810 Petaling Jaya, Selangor", 3.1573298623475234, 101.59005806349877), // Kota Damansara Court
-        Location("Jalan Makmur, Kawasan Perusahaan Sesama, 68100 Batu Caves, Selangor", 3.240364703242958, 101.70037518774967), // Batu Cave Court
-        Location("Lot 965 kg sri delima Jalan kuching pekan, 52000 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur", 3.2037377358676444, 101.66802981330616), // Sri Delima Court
-        Location("Lot 1617, Jalan Genting Kelang, Taman Danau Kota, 53100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur", 3.203099333012631, 101.71846148664879), // Danau Kota Court
-        Location("Level 5, Mines 2, Jalan Mines 2, Mines Wellness City, 43300 Seri Kembangan, Selangor", 3.028749587275076, 101.71527007859078) // Mines Court
+        Location("Persiaran Multimedia, 63100 Cyberjaya, Selangor",2.9272342240047107, 101.64309256580782, 0), // MMU Court
+        Location("Lot 5225, 4, Jalan Puchong Utama 7e, Taman Puchong Utama, 47100 Puchong, Selangor", 2.989928867751879, 101.61626328392457, 10),  // KSL Court Puchong
+        Location("64959, Jalan CJ 15, Kawasan Perindustrian Cheras Jaya, 43200 Seri Kembangan, Selangor", 3.020870690407236, 101.76515845343319, 12),    // Cheras Court
+        Location("Sunsuria Avenue, Persiaran Mohagani, Kota Damansara, 47810 Petaling Jaya, Selangor", 3.1573298623475234, 101.59005806349877, 15), // Kota Damansara Court
+        Location("Jalan Makmur, Kawasan Perusahaan Sesama, 68100 Batu Caves, Selangor", 3.240364703242958, 101.70037518774967, 17), // Batu Cave Court
+        Location("Lot 965 kg sri delima Jalan kuching pekan, 52000 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur", 3.2037377358676444, 101.66802981330616, 13), // Sri Delima Court
+        Location("Lot 1617, Jalan Genting Kelang, Taman Danau Kota, 53100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur", 3.203099333012631, 101.71846148664879, 12), // Danau Kota Court
+        Location("Level 5, Mines 2, Jalan Mines 2, Mines Wellness City, 43300 Seri Kembangan, Selangor", 3.028749587275076, 101.71527007859078, 15) // Mines Court
     };
 
     while (programRunning) {
         double lat, lon;
         Location nearestCourt;
         double minDistance = std::numeric_limits<double>::max();
+        int nearestCourtCounter = 0;
         std::cout << "Enter latitude and longitude (or 'q' to quit): ";
         std::cin >> lat >> lon;
+
+        if(lat < -90 || lat > 90 || lon < -180 || lon > 180) {
+            std::cout << "Invalid latitude or longitude. Please try again." << std::endl;
+            continue;
+        }
 
         if(std::cin.fail()) { // if non-numeric input is given 
             programRunning = false;
@@ -84,15 +94,28 @@ int main() {
         std::cout << std::endl;
 
         for (const Location& court : badmintonCourts) {
+            int counter = 1;
             double distance = haversine(lat, lon, court.getLatitude(), court.getLongitude());
-            std::cout << "Distance to court "<< court.getAddress() <<" : " << distance << " km" << std::endl;
+            std::cout << counter <<") Distance to court "<< court.getAddress() <<" : " << distance << " km" << std::endl;
             if (distance < minDistance) {
+                nearestCourtCounter = counter;
                 minDistance = distance;
                 nearestCourt = court;
             }
         }
-        std::cout << "\nNearest court is at: " << nearestCourt.getAddress() << " which is " << minDistance << " km away." << std::endl;
+        std::cout << nearestCourtCounter <<"\nNearest court is at: " << nearestCourt.getAddress() << " which is " << minDistance << " km away." << std::endl;
         std::cout << "----------------------------------------" << std::endl;
+
+        char choice;
+        int courtChoice;
+        std::cout << "Would you like to book a court? (y/n): ";
+        std::cin >> choice;
+        if (!(choice == 'y' || choice == 'Y')) {
+            std::cout << "Thank you for using the service!" << std::endl;
+            continue;
+        }
+        std::cout << "Choose which court to book (1-" << badmintonCourts.size() << "): ";
+        
     }
     return 0;
 }
